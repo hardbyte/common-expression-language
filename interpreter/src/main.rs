c7310ee0f3b48fe73ac380f3e27bcdbc10eb9c98
Brian Thorne <brian@thorne.link>
2023-07-27 16:43:24 +1200

    🧽 fmt

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -1,4 +1,3 @@
-
 use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::fmt::{Debug, Formatter};
--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,9 +1,7 @@
-
 pub mod ast;
 pub mod parser;
-use chumsky::Parser;
 use chumsky::error::Simple;
-
+use chumsky::Parser;
 
 pub fn parse_cel_expression(input: String) -> Result<ast::Expr, Vec<Simple<char>>> {
     // Expose the internal parser::parse function
